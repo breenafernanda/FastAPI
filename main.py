7e27915904e37909a04ed287093d3707d432cefd
Brena Fernanda Vicente de Souza <42597791+breenafernanda@users.noreply.github.com>
2024-02-07 02:30:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ async def receber_json(dados_json: dict):
                 f'Valor da Proposta: \x1b[31m R$ {valor_proposta}\x1b[32m\n'
             )
 	    try:
-			driver = abrir_navegador()
+		driver = abrir_navegador()	
 	    except Exception as e: print(f'Erro ao abrir navegador: {e}')
 	    
 
