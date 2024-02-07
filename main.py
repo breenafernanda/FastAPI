d1965b9a0331360d3b828f5b6321decbc927698f
Brena Fernanda Vicente de Souza <42597791+breenafernanda@users.noreply.github.com>
2024-02-07 00:51:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+
+@app.get("/financiamento")
+async def root():
+    return {"greeting": "Página de Financiamento"}
+
+
