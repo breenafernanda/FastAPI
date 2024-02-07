9b98b82db314e2659291dd02c9a2f723243db0a0
Brena Fernanda Vicente de Souza <42597791+breenafernanda@users.noreply.github.com>
2024-02-07 02:05:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI, HTTPException
+import uvicorn
 
 app = FastAPI()
 
 # Rota para receber o JSON via método POST
 @app.post("/processar-dados")
 async def receber_json(dados_json: dict):
+    print(f'Dados recebidos: {dados_json}')
     return {"mensagem": "JSON recebido com sucesso", "dados": dados_json}
 
-if __name__ == "__main__":
-    import uvicorn
 
+
+
+
+
+if __name__ == "__main__":
     # Executa o aplicativo usando o servidor Uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
