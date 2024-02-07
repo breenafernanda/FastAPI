dc2be022014fe804696d563b6fd863f51db91b46
Brena Fernanda Vicente de Souza <42597791+breenafernanda@users.noreply.github.com>
2024-02-07 02:19:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,18 @@ async def receber_json(dados_json: dict):
             status_bv = None
             # Seção crítica: Apenas 2 threads podem entrar aqui simultaneamente
             instances_running = semaphore._value
+            cpf = data.get('cpf', 'CPF não especificado')
+            valor_proposta = data.get('valor_proposta', 'Valor não especificado')
             print(f'Buffer: {Handler.buffer}')
+            print(
+                f'\x1b[31m>>> NOVA CHAMADA DE API RECEBIDA <<<<\x1b[32m\n\n    vagas disponíveis no buffer: {instances_running} \n\n'
+                f'Proposta recebida: \x1b[31m{numero_proposta}\x1b[32m\n'
+                f'CPF: \x1b[31m{cpf}\x1b[32m\n'
+                f'Valor da Proposta: \x1b[31m R$ {valor_proposta}\x1b[32m\n'
+            )
+                    
+
+        
             return {"mensagem": "JSON recebido com sucesso", "dados": dados_json}
 
 
