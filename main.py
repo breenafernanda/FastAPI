44e049fb55fbc34d35bcd820975b2b04fae1556e
Brena Fernanda Vicente de Souza <42597791+breenafernanda@users.noreply.github.com>
2024-02-07 01:36:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import random
 
-class API():
-    buffer = 0
+class Handler():
+    buffer = []
+
+def adicionar_buffer(elemento):
+   Handler.buffer.append(elemento)
+
+def remover_buffer(elemento):
+    Handler.buffer.remove(elemento)
     
 app = FastAPI()
 
@@ -11,7 +18,21 @@ async def root():
     return {"greeting": "Hello, World!", "message": f"Welcome to FastAPI!\n\nBuffer = {status_buffer}"}
 
 
-@app.get("/financiamento")
+@app.get("/add1")
 async def root():
-    return {"greeting": "Página de Financiamento", "message":"API FINANCIAMENTO"}
+    numero_escolhido = random.randint(1, 10)
+    adicionar_buffer('elemento1')
+    return {"greeting": "Página de Financiamento", "message":"API FINANCIAMENTO\n ADICIONAR BUFFER 1"}
+    
+@app.get("/add2")
+async def root():
+    numero_escolhido = random.randint(1, 10)
+    adicionar_buffer('elemento2')
+    return {"greeting": "Página de Financiamento", "message":"API FINANCIAMENTO\n ADICIONAR BUFFER 2"}
 
+
+@app.get("/del1")
+async def root():
+    numero_escolhido = random.randint(1, 10)
+    remover_buffer('elemento1')
+    return {"greeting": "Página de Financiamento", "message":"API FINANCIAMENTO\n REMOVER BUFFER"}
