471af843cd73911dad6d7e0dc22eaf9b7d94e6f0
Brena Fernanda Vicente de Souza <42597791+breenafernanda@users.noreply.github.com>
2024-02-07 01:49:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-from fastapi import FastAPI
-import random
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import random
 
 class Handler():
     buffer = []
 
-class DadosParaProcessar(BaseModel):
-    # Defina os campos do modelo aqui
-    nome: str
-    idade: int
-    
 def adicionar_buffer(elemento):
    Handler.buffer.append(elemento)
 
 def remover_buffer(elemento):
     Handler.buffer.remove(elemento)
-    
+
+class DadosParaProcessar(BaseModel):
+    nome: str
+    idade: int
+
 app = FastAPI()
 
 @app.get("/")
@@ -23,28 +22,12 @@ async def root():
     status_buffer = Handler.buffer
     return {"greeting": "Hello, World!", "message": f"Welcome to FastAPI!\n\nBuffer = {status_buffer}"}
 
-
-@app.get("/add1")
-async def root():
-    numero_escolhido = random.randint(1, 10)
-    adicionar_buffer('elemento1')
-    return {"greeting": "Página de Financiamento", "message":"API FINANCIAMENTO\n ADICIONAR BUFFER 1"}
-    
-@app.get("/add2")
-async def root():
-    numero_escolhido = random.randint(1, 10)
-    adicionar_buffer('elemento2')
-    return {"greeting": "Página de Financiamento", "message":"API FINANCIAMENTO\n ADICIONAR BUFFER 2"}
-
-
-@app.get("/del1")
-async def root():
-    numero_escolhido = random.randint(1, 10)
-    remover_buffer('elemento1')
-    return {"greeting": "Página de Financiamento", "message":"API FINANCIAMENTO\n REMOVER BUFFER"}
-
-
-@app.post("/processar-dados")
+@app.post("/processar_dados")
 async def processar_dados(dados: DadosParaProcessar):
-    # Aqui você pode realizar o processamento dos dados recebidos
-    return {"message": f"Dados recebidos com sucesso! Nome: {dados.nome}, Idade: {dados.idade}"}
+    adicionar_buffer({'nome': dados.nome, 'idade': dados.idade})
+    return {"message": "Dados recebidos com sucesso!", "dados": dados.dict()}
+
+@app.get("/ver_buffer")
+async def ver_buffer():
+    status_buffer = Handler.buffer
+    return {"buffer": status_buffer}
