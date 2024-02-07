42a8cc900f50636e307de2be33bcb0d5654cb646
Brena Fernanda Vicente de Souza <42597791+breenafernanda@users.noreply.github.com>
2024-02-07 01:57:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import random
-
-class Handler():
-    buffer = []
-
-def adicionar_buffer(elemento):
-   Handler.buffer.append(elemento)
-
-def remover_buffer(elemento):
-    Handler.buffer.remove(elemento)
-
-class DadosParaProcessar(BaseModel):
-    nome: str
-    idade: int
 
 app = FastAPI()
 
-# @app.get("/")
-# async def root():
-#     status_buffer = Handler.buffer
-#     return {"greeting": "Hello, World!", "message": f"Welcome to FastAPI!\n\nBuffer = {status_buffer}"}
-
+# Rota para receber o JSON via método POST
 @app.post("/processar-dados")
-async def processar_dados(dados: dict):
-    # Lógica de processamento dos dados aqui
-    return {"message": "Dados processados com sucesso", "dados": dados}
-    
-# @app.get("/ver_buffer")
-# async def ver_buffer():
-#     status_buffer = Handler.buffer
-#     return {"buffer": status_buffer}
+async def receber_json(dados_json: dict):
+    return {"mensagem": "JSON recebido com sucesso", "dados": dados_json}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Executa o aplicativo usando o servidor Uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
